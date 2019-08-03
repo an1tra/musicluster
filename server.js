5382a038335a324f4ac2d59533ec76cc18d32e7b
Anitra <anitra.oursky@gmail.com>
2019-08-03 10:28:10 -0400

    Adding passport, bcrypt, updating public folder

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
+// Requiring necessary npm middleware packages 
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("passport");
 
+
+
+// Setting up port
+var PORT = process.env.PORT || 3000;
+
+//Import the models folder
 var db = require("./models");
 
+// Creating express app and configuring middleware 
+//needed to read through our public folder
 var app = express();
-var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false })); //For body parser
+app.use(bodyParser.json());
+app.use(express.static("public"));
+//
+//we are doing a GET to test if our server is working fine
+app.get('/', function(req, res) {    
+  res.send('Welcome to Passport with Sequelize and without HandleBars');
+});
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//
 
 // Handlebars
 app.engine(
@@ -21,9 +48,11 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app, passport);
+require("./routes/htmlRoutes")(app, passport);
+
 
 var syncOptions = { force: false };
 
@@ -45,3 +74,15 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
