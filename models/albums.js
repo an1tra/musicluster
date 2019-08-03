bdd48d68af1fe895dfd8657cb49eba8b1134c2ca
Anitra <anitra.oursky@gmail.com>
2019-08-03 15:46:35 -0400

    add to models and routes

---
+++ models/albums.js
@@ -0,0 +1,20 @@
+//
+// Creating our User model
+//Set it as export because we will need it required on the server
+module.exports = function(sequelize, DataTypes) {
+    var Album = sequelize.define("Album", {
+      // The email cannot be null, and must be a proper email before creation
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    });
+    Album.associate = function(models) {
+        Album.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+  return Album;
+};
