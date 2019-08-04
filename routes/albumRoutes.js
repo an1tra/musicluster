4d4ffe68c98b53d6440020b92de72e52dbc238db
vilagen <vilagen@yahoo.com>
2019-08-04 16:54:22 -0400

    updated albums api

--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -4,17 +4,26 @@ module.exports = function(app) {
     app.get("/api/albums/", function(req, res) {
         var query = {};
         if (req.query.user_id) {
-            query.UserID = req.query.user_id
+            query.UserID = req.query.user_id;
         }
         db.Album.findAll({
             where: query
-        }).then(function(dbAlbum){
-            res.join(dbAlbum)
-        })
+        }).then(function (){
+            for(let i = 0; i < (req.params.name).length; i++) {
+                axios.get("http://ws.audioscrobbler.com/2.0/?method=album.search&album=" 
+                + req.params.name + "&api_key=c37ed91eeb7e472772c6e414db2773a3&format=json&limit=5")
+            .then(function(results) {
+            console.log(results.data);
+            res.json(dbAlbum)
+            })
+        }
     })
+})
     app.post("/api/albums", function(req, res) {
         db.Album.create(req.body).then(function(dbAlbum){
             res.json(dbAlbum)
         })
     })
 };
+
+//for (album names in db) {
