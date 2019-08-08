055e14a3def33c2c2c35c8ae040f9a7fafb6f1d2
vilagen <vilagen@yahoo.com>
2019-08-08 19:40:09 -0400

    Updated with Anitra's information.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models")
 //
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -28,6 +29,7 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be 
   //redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
+    db.Album.findAll({})
     res.render("members", {user: req.user});
   });
 
