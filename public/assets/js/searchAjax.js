644f22f52c0f966054d9c8ab3203afc76f9aad04
J <j.mey.r8@gmail.com>
2019-08-08 21:43:15 -0400

    hid api key for heroku to use

--- public/assets/js/searchAjax.js
+++ public/assets/js/searchAjax.js
@@ -2,65 +2,53 @@ $("#run-search").on("click", function(event) {
     event.preventDefault();
     
     var albumName = $("#search-album").val().trim();
-    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" + albumName + "&api_key=c37ed91eeb7e472772c6e414db2773a3&format=json&limit=5"
     
     $.ajax({
-        url: queryURL,
+        url: "/api/albums/" + albumName,
         method: "GET"
     }).then(function(response) {
         var albumSearch = response.results.albummatches.album[0];
-        console.log(albumTitle)
         if(!albumSearch) {
             alert("We couldn't find that album.")
         } else {
             console.log(albumSearch)
-            clear()
             
+            // variables created from AJAX search
             var albumTitle = albumSearch.name 
-            var pOne = $("<p>").text("Album Name: " + albumSearch.name);
-            var pTwo = $("<p>").text("Album Artist: " + albumSearch.artist);
-            
-            var AlbumCover = $("<img class='cover'>");
-            AlbumCover.attr("src", albumSearch.image[3]["#text"]);
-            AlbumCover.attr("url-link", albumSearch.url);
+            var albumArtist = albumSearch.artist
+            var albumCover = albumSearch.image[3]["#text"]
+            var albumLink = albumSearch.url
 
-            var addButton = $("<button>");
-            addButton.text("Add to Cluster");
-            addButton.attr("id", "albumResult")
+            // moving to Modal
+            $("#albumName").text(albumTitle)
+            $("#albumArtist").text(albumArtist)
 
-            $("#album-section").append(pOne);
-            $("#album-section").append(pTwo);
-            $("#album-section").append(AlbumCover);
-            $("#album-section").append(addButton);
-
-            $(".cover").on("click", function(){
-            window.open(albumSearch.url, "_self");
+            $(".albumCover").attr("src", albumCover)
+            $(".albumCover").attr("url-link", albumLink)
+            $(".albumCover").on("click", function(){
+            window.open(albumLink, "_self");
             })
 
-            $(document).on("click", "#albumResult", function(event){
+            $("#saveToCluster").on("click", function(event){
             event.preventDefault()
-            postAlbum(albumTitle);
-            alert("Album Added")
+            postAlbum(albumTitle, albumArtist, albumCover, albumLink);
+            alert("Album added!")
             location.reload()
             });            
         };
     })
 })
 
-
-function clear() {
-    $("#album-section").empty();
-}
-
-function postAlbum(title) {
-    var newAlbum = {name: title}
+// AJAX POST to create new Albums entry
+function postAlbum(name, artist, cover, link) {
+    var newAlbum = ({name: name,
+                    artist: artist,
+                    cover: cover,
+                    link: link})
     $.ajax("/api/albums", {
         type: "POST",
         data: newAlbum
     }).then(function() {
         console.log(name)
-        alert("Album added!")
     })
   }
-
-$("#clear-all").on("click", clear);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,8 +4,10 @@ var passport = require("../config/passport");
 var multer  = require('multer');
 var crypto = require("crypto");
 var path = require("path");
+var axios = require("axios");
 
-
+// .env for LastFM Key
+var apiKey = process.env.LASTFM_KEY;
 
 var storage = multer.diskStorage({
   destination: 'public/uploads/',
@@ -75,6 +77,15 @@ module.exports = function(app) {
       });
     }
   });
+
+  // Route for using LastFM API
+  app.get("/api/albums/:album", function(req, res) {
+    axios.get("https://ws.audioscrobbler.com/2.0/?method=album.search&album=" + req.params.album + "&api_key=" + apiKey + "&format=json&limit=5").then(function(results){
+      console.log(results.data);
+      res.json(results.data);
+    })
+  });
+
 };
 
 
